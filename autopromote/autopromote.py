40a705f7926d02b55051b1a4c1ee50df0a532568
Harry S <harry.seeber@gusto.com>
2021-10-21 22:01:04 -0600

    that is much easier, thanks 🏹

--- autopromote/autopromote.py
+++ autopromote/autopromote.py
@@ -135,7 +135,7 @@ def get_force_install_time(plist):
 
     patch_day = CONFIG.get("patch_tuesday")
     if isinstance(patch_day, int) and patch_day <= 6 and patch_day >= 0:
-        r = r.shift(days=abs(r.weekday() - patch_day))
+        r = r.shift(weekday=patch_day)
 
     return r.datetime
 
